1117036bc3facd5ceb4ab3d1914ef75d71070412
wait-summer <2091389698@qq.com>
2023-12-18 14:34:37 +0800

    [wait-summer]项目初始化

---
+++ gulimall-coupon/src/test/java/com/waitsummer/gulimall/coupon/GulimallCouponApplicationTests.java
@@ -0,0 +1,13 @@
+package com.waitsummer.gulimall.coupon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallCouponApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
